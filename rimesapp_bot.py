6da313354294cf854c75539ff437ddb22dfe15b1
eman.abdumalik@gmail.com <Rimes>
2022-08-03 00:03:29 +0300

    updated

--- rimesapp_bot.py
+++ rimesapp_bot.py
@@ -1,6 +1,6 @@
 from telegram_menu import BaseMessage, TelegramMenuSession, NavigationHandler
-
-API_KEY = "{api_key}"
+from telegram.parsemode import ParseMode
+API_KEY = "5586703329:AAFyqArzIemhlENC_eZrSojibMLSSAWpiks"
 
 class StartMessage(BaseMessage):
     """Start menu, create all app sub-menus."""
@@ -11,11 +11,18 @@ class StartMessage(BaseMessage):
         """Init StartMessage class."""
         self.nav=navigation
         super().__init__(navigation, StartMessage.LABEL,inlined=True)
-        print(navigation.chat_id)
     def update(self) -> str:
         """Update message content."""
         return "Hello, world!"
     def text_input(self, text: str) -> None:
         """Optional, process text inputs from the keyboard."""
         print(f"Text received: '{str(text)}'")
+        self.nav._bot.send_message(
+            chat_id=self.nav.chat_id,
+            text=text,
+            parse_mode=ParseMode.HTML,
+            reply_markup=None,
+            disable_notification=False,
+        )
+        self.update()
 TelegramMenuSession(API_KEY).start(StartMessage)
